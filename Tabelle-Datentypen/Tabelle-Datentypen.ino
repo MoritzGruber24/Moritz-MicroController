9a7c7ced51762c29329daba0b99ac7e878dc4f0b
Moritz Gruber <moritz.gruber@freenet.de>
2024-10-02 10:52:48 +0200

    Create Tabelle-Datentypen.ino

---
+++ Tabelle-Datentypen/Tabelle-Datentypen.ino
@@ -0,0 +1,30 @@
+
+
+void setup() 
+{
+  Serial.begin (9600);
+
+  Serial.println ("Bitmuster\tuint8_t\t\tint8_t");
+  
+  for (int i = 0; i <= 255; i++)
+    {
+      Serial.print (i, BIN);
+
+      if (i <= 127)
+      {
+        Serial.print ("\t\t");
+      }
+      else
+      {
+        Serial.print ("\t");
+      }
+
+      Serial.print ((uint8_t) i);
+      Serial.print("\t\t");
+      Serial.println ((int8_t)  i);
+
+    }
+}
+
+void loop()
+{}
